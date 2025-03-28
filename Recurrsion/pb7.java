bb03fa9b334f5ccb3e78c631079667acd5dbfc26
Mohit Verma <mohit51341862@gmail.com>
2025-03-28 15:42:55 +0530

    last Occurrence

--- Recurrsion/pb7.java
+++ Recurrsion/pb7.java
@@ -1,13 +1,31 @@
 // write a program to find the last occurence of a number in an array
 
 public class pb7 {
+
+    // method 1 - backward approach
     public static int findLastOccurrence(int[] arr, int key, int i){
         if(i < 0 ) return -1;
         if(arr[i] == key) return i;
         return findLastOccurrence(arr, key, i-1);
     }
+
+
+    // method 2 - forward approach
+    public static int lastOccurrence(int arr[], int key, int i) {
+        if (i == arr.length) {
+            return -1;
+        }
+        int isFound = lastOccurrence(arr, key, i + 1);
+        if (isFound == -1 && arr[i] == key) {
+            return i;
+        }
+        return isFound;
+    }
+
+
     public static void main(String[] args) {
         int[] arr = {1, 8, 3, 4, 4, 5, 5, 5, 6, 7, 8, 8, 9};
-        System.out.println(findLastOccurrence(arr, 5, arr.length - 1));
+        System.out.println(findLastOccurrence(arr, 8, arr.length - 1));
+        System.out.println(lastOccurrence(arr, 8, 0));
     }
 }
