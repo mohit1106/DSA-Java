c521d6ecb3d0844b4230c00b16330d41b199e5bd
Mohit Verma <mohit51341862@gmail.com>
2025-03-28 21:47:35 +0530

    printing x^n

--- Recurrsion/pb8.java
+++ Recurrsion/pb8.java
@@ -1,6 +1,7 @@
 // print x to the power of n
 
 public class pb8 {
+    // normal approach - O(n) complexity
     public static int power(int x, int n){
         if (n == 0) {
             return 1;
@@ -8,7 +9,19 @@ public class pb8 {
         return x * power(x, n-1);
     }
 
+    // optimized approach - O(log n) complexity
+    public static int powerOf(int x, int n){
+        if (n == 0) return 1;
+        int halfPower = powerOf(x, n/2);
+
+        if(n%2 != 0){
+            return x * halfPower * halfPower;
+        }
+        return halfPower * halfPower;
+    }
+
     public static void main(String[] args) {
         System.out.println(power(2, 10));
+        System.out.println(powerOf(2, 10));
     }
 }
