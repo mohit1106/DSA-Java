d4f84095bb48b2bdea84c290b24ac54452b3dfae
Mohit Verma <mohit51341862@gmail.com>
2025-03-28 15:02:01 +0530

    first and last Occurrence

--- Recurrsion/pb6.java
+++ Recurrsion/pb6.java
@@ -1,4 +1,4 @@
-// write a program to find the first occurence of a number in an array
+// write a program to find the first occurence or last Occurence of a number in an array
 
 public class pb6 {
     public static int findFirstOccurrence(int[] arr, int key, int i){
@@ -6,8 +6,14 @@ public class pb6 {
         if(arr[i] == key) return i;
         return findFirstOccurrence(arr, key, i+1);
     }
+    public static int findLastOccurrence(int[] arr, int key, int i){
+        if(i < 0 ) return -1;
+        if(arr[i] == key) return i;
+        return findLastOccurrence(arr, key, i-1);
+    }
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 5, 6, 7, 8, 9};
-        System.out.println(findFirstOccurrence(arr, 8, 0));
+        int[] arr = {1, 8, 3, 4, 4, 5, 5, 5, 6, 7, 8, 8, 9};
+        System.out.println(findFirstOccurrence(arr, 5, 0));
+        System.out.println(findLastOccurrence(arr, 5, arr.length - 1));
     }
 }
