f540328b1282bb18fed83a043b0f5ca5229b869c
Mohit Verma <mohit51341862@gmail.com>
2025-04-01 15:28:47 +0530

    Search in Rotated Sorted Array

--- Divide & Conquer/pb3.java
+++ Divide & Conquer/pb3.java
@@ -42,7 +42,7 @@ public class pb3 {
     
     public static void main(String[] args) {
         int[] arr = {4, 5, 6, 7, 0, 1, 2};
-        int target = 0;
+        int target = 6;
         System.out.println(search(arr, target));  // Output: 4
     }
 }
